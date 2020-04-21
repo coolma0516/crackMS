74a8dc7dcfb4376454a82c3e6c887f476dc957d9
Jason Ma <jason.ma@activenetwork.com>
2020-04-21 15:28:22 +0800

    using API provided by Java.

--- src/com/mageshuo/algo/reversewords/Solution.java
+++ src/com/mageshuo/algo/reversewords/Solution.java
@@ -1,14 +1,14 @@
 package com.mageshuo.algo.reversewords;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Solution {
         public String reverseWords(String s) {
-            StringBuilder sb = new StringBuilder();
-            String[] arr = s.split(" ");
-
-            for(int i = arr.length - 1; i >= 1; i--) {
-                sb.append(arr[i]).append(" ");
-            }
-            sb.append(arr[0]);
-            return sb.toString();
+            s = s.trim();
+            List<String> wordList = Arrays.asList(s.split("\\s+"));
+            Collections.reverse(wordList);
+            return String.join(" ", wordList);
         }
 }
