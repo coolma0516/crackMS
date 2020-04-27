5b7b4dd119d214cf9a40e087406504a3f98978cf
Jason Ma <jason.ma@activenetwork.com>
2020-04-28 07:51:31 +0800

    switch a easy way to random pointer.

--- src/com/mageshuo/algo/randompointer/Solution.java
+++ src/com/mageshuo/algo/randompointer/Solution.java
@@ -34,20 +34,48 @@ public class Solution {
     }
 
     public Node copyRandomList(Node head) {
-        if(head == null) return null;
-
-        Node oldNode = head;
-
-        // creating the new head node
-        Node newNode = new Node(head.val);
-        this.visited.put(oldNode, newNode);
-
-        while(oldNode != null) {
-            newNode.random = this.getClonedNode(oldNode.random);
-            newNode.next = this.getClonedNode(oldNode.next);
-            oldNode = oldNode.next;
-            newNode = newNode.next;
+//        if(head == null) return null;
+//
+//        Node oldNode = head;
+//
+//        // creating the new head node
+//        Node newNode = new Node(head.val);
+//        this.visited.put(oldNode, newNode);
+//
+//        while(oldNode != null) {
+//            newNode.random = this.getClonedNode(oldNode.random);
+//            newNode.next = this.getClonedNode(oldNode.next);
+//            oldNode = oldNode.next;
+//            newNode = newNode.next;
+//        }
+//        return this.visited.get(head);
+        if (head == null) return null;
+        // create new weaved list of original and cloned nodes
+        Node ptr = head;
+        while (ptr != null) {
+            Node newNode = new Node(ptr.val);
+            newNode.next = ptr.next;
+            ptr.next = newNode;
+            ptr = newNode.next;
         }
-        return this.visited.get(head);
+
+        ptr = head;
+        // Now link the random pointer to the new nodes created
+        while (ptr != null) {
+            ptr.next.random = (ptr.random != null) ? ptr.random.next : null;
+            ptr = ptr.next.next;
+        }
+        // unweave the list to get back the original list and the cloned list.
+        Node ptr_old_list = head;
+        Node ptr_new_list = head.next;
+        Node head_new = head.next;
+
+        while(ptr_old_list != null) {
+            ptr_old_list.next = ptr_old_list.next.next;
+            ptr_new_list.next = (ptr_new_list.next != null) ? ptr_new_list.next.next : null;
+            ptr_old_list = ptr_old_list.next;
+            ptr_new_list = ptr_new_list.next;
+        }
+        return head_new;
     }
 }
